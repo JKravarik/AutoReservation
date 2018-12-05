0bef952a527f238174e932f49bfe3a5834d9b29e
Nekodochan <vanessa.janknecht@gmail.com>
2018-12-05 11:14:23 +0100

    WPF First draft

---
+++ AutoReservation.WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoReservation.WPF
+{
+    public partial class MainWindow : Window
+    {
+        CollectionViewSource listingKundenDataView;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listingKundenDataView = (CollectionViewSource)(this.Resources["listingKundenDataView"]);
+        }
+
+        private void OpenAddCustomerWindow(object sender, RoutedEventArgs e)
+        {
+            //AddProductWindow addProductWindow = new AddProductWindow();
+            //addProductWindow.ShowDialog();
+        }
+    }
+}
