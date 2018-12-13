4b0568ffe60269c0578155838fbb4ba566bb85f0
Jana Kravarik <jana.kravarik@hsr.ch>
2018-12-13 11:08:15 +0100

    INotifyPropertyChanged implemented in Dto Classes

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -1,21 +1,96 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
     public class AutoDto : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Marke { get; set; }
-        public int Tagestarif { get; set; }
-        public int Basistarif { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                SetProperty(ref _id, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private string _marke;
+        public string Marke
+        {
+            get { return _marke; }
+            set
+            {
+                SetProperty(ref _marke, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private int _tagestarif;
+        public int Tagestarif
+        {
+            get { return _tagestarif; }
+            set
+            {
+                SetProperty(ref _tagestarif, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private int _basistarif;
+        public int Basistarif
+        {
+            get { return _tagestarif; }
+            set
+            {
+                SetProperty(ref _tagestarif, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
 
         [Timestamp]
-        public byte[] RowVersion { get; set; }
-        public AutoKlasse AutoKlasse { get; set; }
+        private byte[] _rowVersion;
+        public byte[] RowVersion
+        {
+            get { return _rowVersion; }
+            set
+            {
+                SetProperty(ref _rowVersion, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private AutoKlasse _autoKlasse;
+        public AutoKlasse AutoKlasse
+        {
+            get { return _autoKlasse; }
+            set
+            {
+                SetProperty(ref _autoKlasse, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public override string ToString()
         {
--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,21 +1,95 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
     public class ReservationDto : INotifyPropertyChanged
     {
-        public int ReservationsNr { get; set; }
-        public DateTime Von { get; set; }
-        public DateTime Bis { get; set; }
-        public AutoDto Auto { get; set; }
-        public KundeDto Kunde { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _reservationsNr;
+        public int ReservationsNr
+        {
+            get { return _reservationsNr; }
+            set
+            {
+                SetProperty(ref _reservationsNr, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private DateTime _von;
+        public DateTime Von
+        {
+            get { return _von; }
+            set
+            {
+                SetProperty(ref _von, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private DateTime _bis;
+        public DateTime Bis
+        {
+            get { return _von; }
+            set
+            {
+                SetProperty(ref _bis, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private AutoDto _auto;
+        public AutoDto Auto
+        {
+            get { return _auto; }
+            set
+            {
+                SetProperty(ref _auto, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
+
+        private KundeDto _kunde;
+        public KundeDto Kunde
+        {
+            get { return _kunde; }
+            set
+            {
+                SetProperty(ref _kunde, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
 
         [Timestamp]
-        public byte[] RowVersion { get; set; }
+        private byte[] _rowVersion;
+        public byte[] RowVersion
+        {
+            get { return _rowVersion; }
+            set
+            {
+                SetProperty(ref _rowVersion, value);
+                OnPropertyChanged(nameof(ToString));
+            }
+        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public override string ToString()
             => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
